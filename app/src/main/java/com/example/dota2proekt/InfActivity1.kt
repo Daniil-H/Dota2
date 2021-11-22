bf5f1fc2c7398db4a527b84aef0e8676faf30960
Ddd <58085676+studio14cm@users.noreply.github.com>
2021-11-23 02:46:09 +0500

    12

---
+++ app/src/main/java/com/example/dota2proekt/InfActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.dota2proekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inf1)
+    }
+}
